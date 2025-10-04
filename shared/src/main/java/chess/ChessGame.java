63ec8743c1cc64c5578a09fb0713b542dd1e37de
wdawsonv <wdawsonv@byu.edu>
2025-10-03 18:03:40 -0600

    Phase 1: implement getTeamTurn and setTeamTurn

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -10,6 +10,7 @@ import java.util.Collection;
  */
 public class ChessGame {
 
+    private ChessGame.TeamColor teamTurn = TeamColor.WHITE;
     public ChessGame() {
 
     }
@@ -18,7 +19,7 @@ public class ChessGame {
      * @return Which team's turn it is
      */
     public TeamColor getTeamTurn() {
-        throw new RuntimeException("Not implemented");
+        return teamTurn;
     }
 
     /**
@@ -27,7 +28,7 @@ public class ChessGame {
      * @param team the team whose turn it is
      */
     public void setTeamTurn(TeamColor team) {
-        throw new RuntimeException("Not implemented");
+        teamTurn = team;
     }
 
     /**
