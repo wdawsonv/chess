a71f4bf46daa7da595bffb06ed31b2ddbfd6a7bd
wdawsonv <wdawsonv@byu.edu>
2025-12-09 08:08:20 -0700

    Phase 5: implement addUser into ServerFacade.java

---
+++ shared/src/main/java/server/ServerFacade.java
@@ -0,0 +1,78 @@
+package server;
+
+import com.google.gson.Gson;
+import model.*;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublisher;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+public class ServerFacade {
+    private final HttpClient client = HttpClient.newHttpClient();
+    private final String serverUrl;
+
+    public ServerFacade(String url) {
+        serverUrl = url;
+    }
+
+    public UserData addUser(UserData user) throws Exception {
+        var request = buildRequest("POST", "/user", user);
+        var response = sendRequest(request);
+        return handleResponse(response, UserData.class);
+    }
+
+
+
+    private HttpRequest buildRequest(String method, String path, Object body) {
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create(serverUrl + path))
+                .method(method, makeRequestBody(body));
+        if (body != null) {
+            request.setHeader("Content-Type", "application/json");
+        }
+
+        return request.build();
+    }
+
+    private BodyPublisher makeRequestBody(Object request) {
+        if (request != null) {
+            return BodyPublishers.ofString(new Gson().toJson(request));
+        } else {
+            return BodyPublishers.noBody();
+        }
+    }
+
+    private HttpResponse<String> sendRequest(HttpRequest request) throws Exception {
+        try {
+            return client.send(request, BodyHandlers.ofString());
+        } catch (Exception ex) {
+            throw new Exception("if you want more info go look at ServerFacade.java and how to implement ResponseException" + ex.getMessage());
+        }
+    }
+
+    private <T> T handleResponse(HttpResponse<String> response, Class<T> responseClass) throws Exception {
+        var status = response.statusCode();
+        if (!isSuccessful(status)) {
+            var body = response.body();
+            if (body != null) {
+                throw new Exception();
+            }
+
+            throw new Exception("if you want more info go look at ServerFacade.java and how to implement ResponseException");
+        }
+
+        if (responseClass != null) {
+            return new Gson().fromJson(response.body(), responseClass);
+        }
+
+        return null;
+    }
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
+}
