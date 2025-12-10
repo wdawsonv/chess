8a5a78353e0ad19607a94765d1744bcc2561c1b0
wdawsonv <wdawsonv@byu.edu>
2025-12-10 02:55:04 -0700

    Phase 6: copy in WebSocketHandler and ConnectionManager from petshop to experiment on, NOT USABLE

---
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -0,0 +1,62 @@
+package server.websocket;
+
+import com.google.gson.Gson;
+import exception.ResponseException;
+import io.javalin.websocket.WsCloseContext;
+import io.javalin.websocket.WsCloseHandler;
+import io.javalin.websocket.WsConnectContext;
+import io.javalin.websocket.WsConnectHandler;
+import io.javalin.websocket.WsMessageContext;
+import io.javalin.websocket.WsMessageHandler;
+import org.eclipse.jetty.websocket.api.Session;
+import webSocketMessages.Action;
+import webSocketMessages.Notification;
+
+import java.io.IOException;
+
+public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsCloseHandler {
+
+    private final Connectionmanager connections = new ConnectionManager();
+
+    @Override
+    public void handleConnect(WsConnectContext ctx) {
+        System.out.println("Websocket connected");
+        ctx.enableAutomaticPings();
+    }
+
+    @Override
+    public  void handleMessage(WsMessageContext ctx) {
+        try {
+            Action action = new Gson().fromJson(ctx.message(), Action.class);
+            switch (action.type()) {
+                case ENTER -> enter(action.visitorName(), ctx.session);
+                case EXIT -> exit(action.visitorName(), ctx.session);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void handleClose(WsCloseContext ctx) {
+        System.out.println("Websocket closed");
+    }
+
+    private void enter(String visitorName, Session session) throws IOException {
+        connections.add(session);
+        var message = String.format("%s left the shop", visitorName);
+        var notification = new Notification(Notification.Type.DEPARTURE, message);
+        connections.broadcast(session, notification);
+        connections.remove(session);
+    }
+
+    public void makeNoise(String petName, String sound) throws ResponseException {
+        try {
+            var message = String.format("%s says %s", petName, sound);
+            var notification = new Notification(Notification.Type.NOISE, message);
+            connections.broadcast(null, notification);
+        } catch (Exception ex) {
+            throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
+        }
+    }
+}
