cce0185265e459de4ebe0b06a44631f53bb7b7c7
wdawsonv <wdawsonv@byu.edu>
2026-01-23 20:25:44 -0700

    Add isEqual overrides and basic bishop movement functionality

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -54,9 +55,95 @@ public class ChessPiece {
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         ChessPiece piece = board.getPiece(myPosition);
+
         if (piece.getPieceType() == PieceType.BISHOP) {
-            return List.of(new ChessMove(new ChessPosition(5, 4), new ChessPosition(1, 8), null));
+            return bishopMoves(board, myPosition);
         }
         return List.of();
+    }
+
+    private Collection<ChessMove> bishopMoves(ChessBoard board, ChessPosition myPosition) {
+        Collection<ChessMove> finalMoveList = new ArrayList<>();
+
+        finalMoveList.addAll(diagUpLeftMoves(board, myPosition));
+        finalMoveList.addAll(diagUpRightMoves(board, myPosition));
+        finalMoveList.addAll(diagDownLeftMoves(board, myPosition));
+        finalMoveList.addAll(diagDownRightMoves(board, myPosition));
+
+        return finalMoveList;
+    }
+
+    //Diagonal Move Checkers
+    private Collection<ChessMove> diagUpLeftMoves(ChessBoard board, ChessPosition myPosition) {
+        int testRow = myPosition.getRow() + 1;
+        int testCol = myPosition.getColumn() - 1;
+        Collection<ChessMove> tempMoveList = new ArrayList<>();
+
+        while (testRow <= 8 && testCol >= 1) {
+            ChessPosition testEndPosition = new ChessPosition(testRow, testCol);
+            ChessMove possibleMove = new ChessMove(myPosition, testEndPosition, null);
+
+            if (board.getPiece(testEndPosition) == null) {
+                tempMoveList.add(possibleMove);
+            }
+            testRow++;
+            testCol--;
+        }
+
+        return tempMoveList;
+    }
+    private Collection<ChessMove> diagUpRightMoves(ChessBoard board, ChessPosition myPosition) {
+        int testRow = myPosition.getRow() + 1;
+        int testCol = myPosition.getColumn() + 1;
+        Collection<ChessMove> tempMoveList = new ArrayList<>();
+
+        while (testRow <= 8 && testCol <= 8) {
+            ChessPosition testEndPosition = new ChessPosition(testRow, testCol);
+            ChessMove possibleMove = new ChessMove(myPosition, testEndPosition, null);
+
+            if (board.getPiece(testEndPosition) == null) {
+                tempMoveList.add(possibleMove);
+            }
+            testRow++;
+            testCol++;
+        }
+
+        return tempMoveList;
+    }
+    private Collection<ChessMove> diagDownLeftMoves(ChessBoard board, ChessPosition myPosition) {
+        int testRow = myPosition.getRow() - 1;
+        int testCol = myPosition.getColumn() - 1;
+        Collection<ChessMove> tempMoveList = new ArrayList<>();
+
+        while (testRow >= 1 && testCol >= 1) {
+            ChessPosition testEndPosition = new ChessPosition(testRow, testCol);
+            ChessMove possibleMove = new ChessMove(myPosition, testEndPosition, null);
+
+            if (board.getPiece(testEndPosition) == null) {
+                tempMoveList.add(possibleMove);
+            }
+            testRow--;
+            testCol--;
+        }
+
+        return tempMoveList;
+    }
+    private Collection<ChessMove> diagDownRightMoves(ChessBoard board, ChessPosition myPosition) {
+        int testRow = myPosition.getRow() - 1;
+        int testCol = myPosition.getColumn() + 1;
+        Collection<ChessMove> tempMoveList = new ArrayList<>();
+
+        while (testRow >= 1 && testCol <= 8) {
+            ChessPosition testEndPosition = new ChessPosition(testRow, testCol);
+            ChessMove possibleMove = new ChessMove(myPosition, testEndPosition, null);
+
+            if (board.getPiece(testEndPosition) == null) {
+                tempMoveList.add(possibleMove);
+            }
+            testRow--;
+            testCol++;
+        }
+
+        return tempMoveList;
     }
 }
