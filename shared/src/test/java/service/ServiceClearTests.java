9d3096386133a2ba2ad703915cf81400dc41197a
wdawsonv <wdawsonv@byu.edu>
2026-03-11 20:12:22 -0600

    Phase 3: implement clear db test (broken waiting for adding and retrieving)

---
+++ shared/src/test/java/service/ServiceClearTests.java
@@ -0,0 +1,24 @@
+package service;
+
+import dataaccess.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ServiceClearTests {
+
+    @Test
+    @DisplayName("Clear Database")
+    public void clearDatabase() {
+        UserService.clear();
+        GameService.clear();
+        AuthService.clear();
+        Assertions.assertAll(
+                "All services should be clear",
+                () -> Assertions.assertNull(MemoryUserDAO.users),
+                () -> Assertions.assertNull(MemoryGameDAO.games),
+                () -> Assertions.assertNull(MemoryAuthDAO.auths)
+        ); //this needs to be changes once we implement getting info back yay
+
+    }
+}
