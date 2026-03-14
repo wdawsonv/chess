b664155ec87fb3a42e6d5e54b63918c8f9e105c0
wdawsonv <wdawsonv@byu.edu>
2026-03-13 20:22:45 -0600

    Phase 4: fix issue with createStatements  in DatabaseUserDao, some tests pass

--- server/src/main/java/dataaccess/DatabaseUserDAO.java
+++ server/src/main/java/dataaccess/DatabaseUserDAO.java
@@ -95,9 +95,9 @@ public class DatabaseUserDAO implements UserDAO {
     private final String[] createStatements = {
             """
             CREATE TABLE IF NOT EXISTS users (
-            `username` string NOT NULL,
-            `password` string NOT NULL,
-            `email` string NOT NULL,
+            `username` VARCHAR(255) NOT NULL,
+            `password` VARCHAR(255) NOT NULL,
+            `email` VARCHAR(255) NOT NULL,
             PRIMARY KEY (`username`)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
             """
