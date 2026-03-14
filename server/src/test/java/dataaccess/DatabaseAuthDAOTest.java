d65e25f5de4f729b9180b02873971ad174a86164
wdawsonv <wdawsonv@byu.edu>
2026-03-13 22:37:39 -0600

    Phase 4: add unit tests

---
+++ server/src/test/java/dataaccess/DatabaseAuthDAOTest.java
@@ -0,0 +1,54 @@
+package dataaccess;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseAuthDAOTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void clearAuthDB() {
+    }
+
+    @Test
+    void addAuthPositive() {
+    }
+
+    @Test
+    void findAuthPositive() {
+    }
+
+    @Test
+    void removeAuthPositive() {
+    }
+
+    @Test
+    void findUsernamePositive() {
+    }
+
+    @Test
+    void addAuthNegative() {
+    }
+
+    @Test
+    void findAuthNegative() {
+    }
+
+    @Test
+    void removeAuthNegative() {
+    }
+
+    @Test
+    void findUsernameNegative() {
+    }
+}
---
+++ server/src/test/java/dataaccess/DatabaseGameDAOTest.java
@@ -0,0 +1,52 @@
+package dataaccess;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseGameDAOTest {
+
+    @Test
+    void clearGameDB() {
+    }
+
+    @Test
+    void verifyGameExistsPositive() {
+    }
+
+    @Test
+    void getGamePositive() {
+    }
+
+    @Test
+    void createGamePositive() {
+    }
+
+    @Test
+    void listGamesPositive() {
+    }
+
+    @Test
+    void addPlayerPositive() {
+    }
+
+    @Test
+    void verifyGameExistsNegative() {
+    }
+
+    @Test
+    void getGameNegative() {
+    }
+
+    @Test
+    void createGameNegative() {
+    }
+
+    @Test
+    void listGamesNegative() {
+    }
+
+    @Test
+    void addPlayerNegative() {
+    }
+}
