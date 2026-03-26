3c5b82860f5e52f80a523e94b598ead7b6202297
wdawsonv <wdawsonv@byu.edu>
2026-03-25 18:57:29 -0600

    Phase 5: change the way buildrequest works to be able to handle authTokens (petshop could not unfortunately)

--- shared/src/main/server/ServerFacade.java
+++ shared/src/main/server/ServerFacade.java
@@ -19,12 +19,38 @@ public class ServerFacade {
         serverUrl = url;
     }
 
-    private HttpRequest buildRequest(String method, String path, Object body) {
+    public void clearUsers() throws ResponseException {
+        var request = buildRequest("DELETE", "/db", null, null);
+        sendRequest(request);
+    }
+
+    public RegisterResult register(RegisterRequest registerRequest) throws ResponseException {
+        var request = buildRequest("POST", "/user", registerRequest, null);
+        var response = sendRequest(request);
+        return handleResponse(response, RegisterResult.class);
+    }
+
+    public LoginResult login(LoginRequest loginRequest) throws ResponseException {
+        var request = buildRequest("POST", "/session", loginRequest, null);
+        var response = sendRequest(request);
+        return handleResponse(response, LoginResult.class);
+    }
+
+    public void logout(String authToken) throws ResponseException {
+        var request = buildRequest("DELETE", "/session", null, authToken);
+        sendRequest(request);
+    }
+
+    private HttpRequest buildRequest(String method, String path, Object body, String header) {
         var request = HttpRequest.newBuilder()
                 .uri(URI.create(serverUrl + path))
                 .method(method, makeRequestBody(body));
         if (body != null) {
             request.setHeader("Content-type", "application/json");
+        }
+
+        if (header != null) {
+            request.setHeader("authorization", header);
         }
         return request.build();
     }
